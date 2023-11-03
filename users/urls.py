714fb972411a6b4350fbea4f0c16ded35a915a48
BasilZap <zapr9@yandex.ru>
2023-11-03 20:06:54 +0300

    Исправлено название

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from users.views import UserCreateAPIView
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('register/', UserCreateAPIView.as_view(), name='user-create'),
+    path('register/', UserCreateAPIView.as_view(), name='user_create'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     ]
