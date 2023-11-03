0daa0b07f3d3d53acb7b72450d5efedf4969aba5
BasilZap <zapr9@yandex.ru>
2023-11-03 20:04:27 +0300

    Изменена модель пользователя. Добавлено поле "ID телеграм"

--- users/models.py
+++ users/models.py
@@ -12,6 +12,7 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='Телефон', **NULLABLE)
     city = models.CharField(max_length=50, verbose_name='Город', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
+    telegram_id = models.CharField(max_length=15, verbose_name='ID телеграм', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
